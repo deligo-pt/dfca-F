fd175eabd5dd20a9e4d00cf0abeb60c4ca45b92e
nisharga <kabirnisharga@gmail.com>
2025-12-06 17:50:15 +0600

    feat: map-intrigration

--- DeligoCustomer/src/screens/EditProfileScreen.js
+++ DeligoCustomer/src/screens/EditProfileScreen.js
@@ -14,6 +14,7 @@ import { Ionicons } from "@expo/vector-icons";
 import { useTheme } from "../utils/ThemeContext";
 import { getUserData } from "../utils/auth";
 import { useLanguage } from "../utils/LanguageContext";
+import { LocationDetails } from "../components/Profile";
 
 const EditProfileScreen = ({ navigation, route }) => {
   const { t } = useLanguage();
@@ -465,6 +466,17 @@ const EditProfileScreen = ({ navigation, route }) => {
               </View>
             )}
           </View>
+        </View>
+
+        {/* LocationDetails */}
+
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "red",
+          }}
+        >
+          <LocationDetails />
         </View>
 
         {/* Save Button */}
